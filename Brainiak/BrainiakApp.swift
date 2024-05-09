769d1f2dd3c601197dee13771fb61e5f8053e522
mzmddi <110358757+mzmddi@users.noreply.github.com>
2024-05-09 12:15:41 -0400

    Initial Commit

---
+++ Brainiak/BrainiakApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainiakApp.swift
+//  Brainiak
+//
+//  Created by M on 2024-05-09.
+//
+
+import SwiftUI
+
+@main
+struct BrainiakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
